97853b5a91dd1dce5f70235d83e43740ae5688d7
ZETA <ben.r.dominguez@gmail.com>
2022-01-07 14:50:42 -0800

    Updated Assets/script.js allowing questions to display when 'Start Quiz' button is pressed.

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,22 @@
+//start button
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+//question container
 const questionContainerEl = document.getElementById('question-container')
+//question
 const questionEl = document.getElementById('question')
+//answer button
 const answerButtonsEl = document.getElementById('answer-button')
 
 var shuffledQuestions, currentQuestionIndex
 
+//timer
+var timeCount = 60;
+
+//game will start when i click the start button
 startButton.addEventListener('click', startGame)
 
+//When I click start game the game starts with a random question
 function startGame() {
     console.log('Started game')
     startButton.classList.add('hide')
@@ -16,6 +26,7 @@ function startGame() {
     setNextQuestion()
 }
 
+//When answer is clicked game will show questions
 function setNextQuestion() {
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
@@ -25,68 +36,79 @@ function showQuestion(question) {
     question.answers.forEach(answer => {
         const button = document.createElement('button')
         button.innerText = answer.text
-        button.classList.add('btn')
+            button.classList.add('btn', 'btn-grid')
         if (answer.correct) {
             button.dataset.correct = answer.correct
         }
         button.addEventListener('click', selectAnswer)
-        answerButtonsEl.appendChild(button)
+        questionEl.appendChild(button)
     })
 }
 
-function selectAnswer() {
+//if the answer is incorrect
+function selectAnswer(e) {
 
 }
+//then time is subtracted from the timer
+
+//when all questions are answered or if timer reaches 0 the game is over
+
+//when the game is over i can save my initials and score
 
 var questions = [
     {
         no: 1,
         question: 'Placeholder',
         answers: [
-            "Correct Answer",
-            "False Answer",
-            "False Answer",
-            "False Answer"
+            { text: "Correct Answer", correct: true },
+            { text: "False Answer", correct: false},
+            { text: "False Answer", correct: false},
+            { text: "False Answer", correct: false}
         ]
     },
     {
         no: 2,
         question: 'Placeholder 2',
         answers: [
-            "Correct Answer",
-            "False Answer",
-            "False Answer",
-            "False Answer",
+            { text: "Correct Answer", correct: true },
+            { text: "False Answer", correct: false},
+            { text: "False Answer", correct: false},
+            { text: "False Answer", correct: false}
         ]
     },
     {
         no: 3,
         question: 'Placeholder 3',
         answers: [
-            "Correct Answer",
-            "False Answer",
-            "False Answer",
-            "False Answer",
+            { text: "Correct Answer", correct: true },
+            { text: "False Answer", correct: false},
+            { text: "False Answer", correct: false},
+            { text: "False Answer", correct: false}
         ]
     },
     {
         no: 4,
         question: 'Placeholder 4',
         answers: [
-            "Correct Answer",
-            "False Answer",
-            "False Answer",
-            "False Answer",
+            { text: "Correct Answer", correct: true },
+            { text: "False Answer", correct: false},
+            { text: "False Answer", correct: false},
+            { text: "False Answer", correct: false}
         ]
     },
     {
         no: 5,
         question: 'Placeholder',
         answers: [
-            "Correct Answer",
-            "False Answer",
-            "False Answer",
-            "False Answer",
+            { text: "Correct Answer", correct: true },
+            { text: "False Answer", correct: false},
+            { text: "False Answer", correct: false},
+            { text: "False Answer", correct: false}
         ]
     },
 ]
+
+function selectAnswer() {
+
+}
+
